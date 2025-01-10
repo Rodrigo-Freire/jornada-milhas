0034f020d0dc60110a988f0db24ea1a8bb9aee6f
Rodrigo <rodrigo.freire@msn.com>
2025-01-10 10:15:24 -0300

    Dropdown de origem e destino

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,16 +1,25 @@
-import { Injectable } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Injectable } from "@angular/core";
+import { Form, FormControl, FormGroup } from "@angular/forms";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root",
 })
 export class FormBuscaService {
+	formBusca: FormGroup;
 
-  formBusca: FormGroup
+	constructor() {
+		this.formBusca = new FormGroup({
+			somenteIda: new FormControl(false),
+			origem: new FormControl(null),
+			destino: new FormControl(null),
+		});
+	}
 
-  constructor() {
-    this.formBusca = new FormGroup({
-      somenteIda: new FormControl(false)
-    })
-  }
+	obertControle(nome: string): FormControl {
+		const control = this.formBusca.get(nome);
+		if (!control) {
+			throw new Error(`FormControl com nome "${nome}" não existe`);
+		}
+		return control as FormControl;
+	}
 }
--- src/app/core/services/unidade-federativa.service.ts
+++ src/app/core/services/unidade-federativa.service.ts
@@ -1,31 +1,27 @@
-import { Injectable } from '@angular/core';
-import { Observable, shareReplay } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { UnidadeFederativa } from '../types/types';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { Observable, shareReplay } from "rxjs";
+import { environment } from "src/environments/environment";
+import { UnidadeFederativa } from "../types/types";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root",
 })
 export class UnidadeFederativaService {
-  private apiUrl: string = environment.apiUrl
-  private cache$?: Observable<UnidadeFederativa[]>
+	private apiUrl: string = environment.apiUrl;
+	private cache$?: Observable<UnidadeFederativa[]>;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+	constructor(private http: HttpClient) {}
 
-  listar(): Observable<UnidadeFederativa[]> {
-    if (!this.cache$) {
-      this.cache$ = this.requestEstados().pipe(
-        shareReplay(1)
-      );
-    }
+	listar(): Observable<UnidadeFederativa[]> {
+		if (!this.cache$) {
+			this.cache$ = this.requestEstados().pipe(shareReplay(1));
+		}
 
-    return this.cache$;
-  }
+		return this.cache$;
+	}
 
-  private requestEstados(): Observable<UnidadeFederativa[] {
-    return this.http.get<UnidadeFederativa[]>(`${this.apiUrl}/estados`);
-  }
+	private requestEstados(): Observable<UnidadeFederativa[]> {
+		return this.http.get<UnidadeFederativa[]>(`${this.apiUrl}/estados`);
+	}
 }
